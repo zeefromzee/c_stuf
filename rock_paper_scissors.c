913a42aded0adac3e66d0d1da2c2454ffdc26f94
zee <zeelg322@gmail.com>
2025-09-23 22:18:48 +0530

    Update rock_paper_scissors.c

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -3,51 +3,73 @@
 #include <time.h>
 #include <string.h>
 
-int main(){
+int main() {
+    // Seed the random number generator with the current time
     srand(time(NULL));
+
+    // Variables to store user and computer choices
     char user_choice[50];
     char comp_choice[50];
-    char choices[3][100]={"Rock", "Paper", "Scissors"};
-    printf("Enter your choice(Rock/Paper/Scissors): ");
+
+    // Array of possible choices
+    char choices[3][100] = {"Rock", "Paper", "Scissors"};
+
+    // Take input from user
+    printf("Enter your choice (Rock/Paper/Scissors): ");
     scanf("%s", user_choice);
-    
-    int choice_index= rand() % 3;
+
+    // Generate computer's random choice
+    int choice_index = rand() % 3; // Random number between 0-2
     strcpy(comp_choice, choices[choice_index]);
+
+    // Display computer's choice
     printf("Computer chose: %s\n", comp_choice);
-    
-  if (strcmp(user_choice, comp_choice)==0){
-      printf("It is a draw :/\n");
-  } 
-      
-  else if (strcmp(user_choice, choices[0])==0){
-      if (strcmp(user_choice,choices[1])==0){
-        printf("Computer wins :(\n");
-       
-  
-  }else{
-      printf("You win\n");
-    }
-  }
 
-  else if (strcmp(user_choice, choices[1])==0){
-      if (strcmp(comp_choice, choices[2])==0){
-        printf("Computer wins :p\n");
-    } else{
-      printf("You win :D\n");
+    // Compare user choice and computer choice
+    if (strcmp(user_choice, comp_choice) == 0) {
+        // If both choices are same
+        printf("It is a draw :/\n");
     }
 
-  }
-
-    else if (strcmp(user_choice, choices[2])==0){
-    if (strcmp(comp_choice, choices[0])==0){
-      printf("Computer wins\n");
+    // If user chooses Rock
+    else if (strcmp(user_choice, choices[0]) == 0) {
+        if (strcmp(comp_choice, choices[1]) == 0) {
+            // Rock vs Paper → Computer wins
+            printf("Computer wins :(\n");
+        } else {
+            // Rock vs Scissors → User wins
+            printf("You win!\n");
+        }
     }
-  } else {
-    printf ("You win!! :)\n");
-  } 
-  
+
+    // If user chooses Paper
+    else if (strcmp(user_choice, choices[1]) == 0) {
+        if (strcmp(comp_choice, choices[2]) == 0) {
+            // Paper vs Scissors → Computer wins
+            printf("Computer wins :p\n");
+        } else {
+            // Paper vs Rock → User wins
+            printf("You win :D\n");
+        }
+    }
+
+    // If user chooses Scissors
+    else if (strcmp(user_choice, choices[2]) == 0) {
+        if (strcmp(comp_choice, choices[0]) == 0) {
+            // Scissors vs Rock → Computer wins
+            printf("Computer wins >:)\n");
+        } else {
+            // Scissors vs Paper → User wins
+            printf("You win!!\n");
+        }
+    }
+
+    // If user enters something invalid
+    else {
+        printf("Invalid choice! Please type Rock, Paper, or Scissors.\n");
+    }
+
+    return 0;
 }
-
-
 
 
